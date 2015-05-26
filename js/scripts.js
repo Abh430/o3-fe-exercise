5935a1b780c35e3965c29f8184b0a6d63fc1524f
Andrew Hunter <abh430@gmail.com>
2015-05-25 22:17:15 -0400

    Created fluid layout and mobile nav

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	//mobile nav event listener
+	$('#menu-link').click(function(){
+		$('nav ul').slideToggle('active-menu');
+	});
+
+	//image swap event listener
+	$('img').click(function(){
+		$('.active-hero')
+
+	})
+})
